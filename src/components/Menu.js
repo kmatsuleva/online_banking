7ff87d03f98c148681138ca3be0c95771f3708ff
Katerina Matsuleva <katyushka17091998@gmail.com>
2018-08-15 14:09:00 +0300

    Added a component that includes all items of menu

---
+++ src/components/Menu.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MenuItem from "./MenuItem";
+
+const Menu = () => (
+  <ul>
+    <MenuItem item="CORPORATE CLIENTS" />
+    <MenuItem item="ABOUT US" />
+    <MenuItem item="ONLINE BANKING" />
+  </ul>
+);
+
+export default Menu;
