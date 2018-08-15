3a5d7d3fa6b5abcd27de88c7eef35dd8eae406fa
Katerina Matsuleva <katyushka17091998@gmail.com>
2018-08-15 14:05:34 +0300

    Added a presentational component that includes a form

---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Form = ({ label }) => (
+  <div>
+    <form action="#">
+      <label> {label} </label>
+      <br />
+      <input />
+    </form>
+  </div>
+);
+
+export default Form;
